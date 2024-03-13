39a86002a072562f9803f536f19f746cb8865a95
PAJK87 <pajk87@gmail.com>
2024-03-13 15:53:16 -0400

    Moving backend code from main to backend branch. Will push to main when functional

--- book-buddy/src/main/java/com/bookbuddy/bookbuddy/CreatedExceptions/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bookbuddy.bookbuddy.CreatedExceptions;
-
-public class BookNotFoundException extends RuntimeException {
-
-    public BookNotFoundException(Long id) {
-        super("Could not find book " + id);
-    }
-
-}
--- book-buddy/src/main/java/com/bookbuddy/bookbuddy/Repositories/BookCollectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookbuddy.bookbuddy.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookbuddy.bookbuddy.Entities.BookCollection;
-
-interface BookCollectionRepository extends JpaRepository<BookCollection, Long>{
-    
-}
