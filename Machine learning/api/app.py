118278ec78547d92f31101bc57209e10895bb803
Dhanasri <120515043+Dhanasrisalla@users.noreply.github.com>
2024-04-20 14:26:52 -0400

    app.py

--- Machine learning/api/app.py
+++ Machine learning/api/app.py
@@ -5,31 +5,24 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes and origins
+CORS(app)
 
-# Load and prepare your dataset
 df = pd.read_csv('cleaned_goodreads_data.csv')
 df.dropna(subset=['cleaned_description'], inplace=True)
-
-# Reset the index of the dataframe to ensure alignment
 df.reset_index(drop=True, inplace=True)
 
 tfidf = TfidfVectorizer()
 mtx = tfidf.fit_transform(df['cleaned_description'])
 sim = cosine_similarity(mtx)
 
-# Create a case-insensitive mapping of book titles to indices
 idx_map = pd.Series(df.index, index=df['Book'].str.lower()).drop_duplicates()
 
 @app.route('/recommendations/<book_name>', methods=['GET'])
 def recommendations(book_name):
     n = request.args.get('n', default=10, type=int)
-    book_name = book_name.lower()  # Convert the input to lowercase for case-insensitive matching
-
-    print(f"Book name: {book_name}")  # Debug print statement
+    book_name = book_name.lower()
 
     if book_name not in idx_map:
-        print(f"Book not found: {book_name}")  # Debug print statement
         return jsonify({"error": "Book not found"}), 404
 
     ix = idx_map[book_name]
@@ -40,22 +33,17 @@ def recommendations(book_name):
     book_ix = [i[0] for i in scores]
     recommended_books = df.iloc[book_ix, [1, 2, 3, 4, 5, 6]].to_dict('records')
 
-    print(f"Recommended books: {recommended_books}")  # Debug print statement
-
     return jsonify(recommended_books)
 
 @app.route('/search', methods=['GET'])
 def search_books():
     query = request.args.get('q', '').lower()
-    print(f"Search query: {query}")  # Debug print statement
 
     if not query:
         return jsonify([])
 
     search_results = df[df['Book'].str.lower().str.contains(query, case=False)]
     search_results = search_results[['Book', 'Author', 'Description', 'Genres', 'Avg_Rating', 'Num_Ratings']].to_dict('records')
-
-    print(f"Search results: {search_results}")  # Debug print statement
 
     return jsonify(search_results)
 
